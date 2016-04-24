21e61a605752df1430da6a0517ae34a5ba93e4cc
Ben Lerner <ben@caffeinatedanalytics.com>
2016-04-24 17:32:29 -0400

    added link to tutorial

--- filter.js
+++ filter.js
@@ -1,3 +1,5 @@
+// from https://developer.chrome.com/extensions/examples/tutorials/getstarted/popup.js
+
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
